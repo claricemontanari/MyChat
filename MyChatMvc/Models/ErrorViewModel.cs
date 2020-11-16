6f3b46e9fbeb8ef01da407b4aaf952ccc5c4cacc
Clarice Montanari <clarice.montanari@gmail.com>
2020-11-16 18:46:30 +0000

    Add project files.

---
+++ MyChatMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyChatMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
